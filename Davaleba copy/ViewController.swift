83312e26798cf852496a73c2f6a21c7061be30ef
Lado <Tsivtsivadzelado@yahoo.com>
2020-12-02 10:05:28 -0800

    geoApp

---
+++ Davaleba copy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Davaleba
+//
+//  Created by Ladolado3911 on 12/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
